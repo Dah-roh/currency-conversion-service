007fcebdb4d402f74224a5fc6b1e86e5de1320ba
dizsart <darogadibia@gmail.com>
2020-06-12 21:47:39 +0100

    add the currencyExchangeServiceProxy

---
+++ src/main/java/com/spring/currency_conversion_service/service/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,14 @@
+package com.spring.currency_conversion_service.service;
+
+import com.spring.currency_conversion_service.CurrencyConversionBean;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name="forex-service", url="localhost:9000")
+public interface CurrencyExchangeServiceProxy {
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversionBean retrieveExchangeValue
+            (@PathVariable("from") String from, @PathVariable("to") String to);
+}
+
