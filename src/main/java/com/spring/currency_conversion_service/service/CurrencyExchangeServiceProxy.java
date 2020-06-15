94c0fa8a9b5d85ad5236f882edfa00312ac1dd4a
dizsart <darogadibia@gmail.com>
2020-06-15 10:49:49 +0100

    add configuration of the ribbon load balancing for the services

--- src/main/java/com/spring/currency_conversion_service/service/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/spring/currency_conversion_service/service/CurrencyExchangeServiceProxy.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="forex-service", url="localhost:9000")
+@FeignClient(name="forex-service")
 @RibbonClient(name="forex-service")
 public interface CurrencyExchangeServiceProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
